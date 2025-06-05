f64bce600f929c6df551ef09a4be710eaada6d49
duchai922001 <Duchai757@gmail.com>
2025-06-05 14:28:41 +0700

    fix

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -2,7 +2,7 @@ import axiosInstance from "./main.service";
 
 export const UserService = {
   register: async (payload: any) => {
-    const response = await axiosInstance.post(`/users/register`, payload);
+    const response = await axiosInstance.post(`/users`, payload);
     return response;
   },
   login: async (payload: any) => {
